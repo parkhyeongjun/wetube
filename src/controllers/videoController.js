99b104a93d835fbeef0547f3450a92a709d045b3
parkhyeongjun <alvin.jun12345@gmail.com>
2023-04-03 01:56:37 +0900

    6.0

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,43 @@
-export const trending = (req, res) => res.render("home");
+let videos = [{
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1
+},
+{
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2
+},
+{
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3
+}]
+export const trending = (req, res) => {
+
+    res.render("home", {pageTitle: "Home", videos})
+};
+
+export const see = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1]
+    return res.render("watch", {pageTitle: `Watching ${video.title}`})
+};
+
 export const edit = (req, res) => res.send("Edit");
+
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => res.send(`Watch video #${req.params.id}`);
+
+
 export const deleteVideo = (req, res) => res.send("Delete Video");
+
 export const upload = (req, res) => res.send("Upload Video");
